738eb85d5bb3a065d661781550ffd3c9a8b0037b
Toni Schmidbauer <toni@stderr.at>
2021-08-28 21:15:37 +0200

    import initial if-else example

---
+++ java/if-else/src/main/java/at/stderr/if-else/App.java
@@ -0,0 +1,13 @@
+package at.stderr.if-else;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
